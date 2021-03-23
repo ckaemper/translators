5e57def828d9ee78d45839777c33d10bf46ba52f
Christian Kämper <christian.kaemper@interrogare.de>
2021-03-23 16:29:26 +0100

    Add functionality for getSearchResults

--- Heise+.js
+++ Heise+.js
@@ -2,21 +2,21 @@
 	"translatorID": "6cb757b0-ba22-4350-b43d-bb4a2690e0ee",
 	"label": "Heise+",
 	"creator": "Christian Kämper",
-	"target": "^https?://www\\.heise\\.de/select/(ct|ix)/archiv/\\d{4}/\\d{1,2}/",
+	"target": "^https?://www\\.heise\\.de/select/(ct|ix)/archiv/\\d{4}/\\d{1,2}",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-03-19 09:52:52"
+	"lastUpdated": "2021-03-23 09:22:02"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2020 YOUR_NAME <- TODO
-	
+	Copyright © 2020 Christian Kämper
+
 	This file is part of Zotero.
 
 	Zotero is free software: you can redistribute it and/or modify
@@ -50,17 +50,15 @@ function detectWeb(doc, url) {
 function getSearchResults(doc, checkOnly) {
 	var items = {};
 	var found = false;
-	// TODO: adjust the CSS selector
-	var rows = doc.querySelectorAll('h2>a.title[href*="/article/"]');
+	var index = 0;
+	var rows = doc.querySelectorAll('.issue__files__bibtex');
 	for (let row of rows) {
-		// TODO: check and maybe adjust
-		let href = row.href;
-		// TODO: check and maybe adjust
-		let title = ZU.trimInternal(row.textContent);
-		if (!href || !title) continue;
+		let bibtex = ZU.trimInternal(row.textContent);
+		if (!bibtex) continue;
 		if (checkOnly) return true;
 		found = true;
-		items[href] = title;
+		items[index] = bibtex;
+		index++;
 	}
 	return found ? items : false;
 }
@@ -68,39 +66,50 @@ function getSearchResults(doc, checkOnly) {
 function doWeb(doc, url) {
 	if (detectWeb(doc, url) == "multiple") {
 		Zotero.selectItems(getSearchResults(doc, false), function (items) {
-			if (items) ZU.processDocuments(Object.keys(items), scrape);
+			if (!items) return true;
+			for (var i in items) {
+				let page = items[i].match(/@article\{(\d+)/)[1];
+				let pageUrl = url + (url.endsWith("/") ? "" : "/") + 'seite-' + page;
+				scrape(items[i], pageUrl);
+			}
 		});
 	}
 	else {
-		scrape(doc, url);
+		const bibtex = (ZU.trimInternal(doc.getElementsByClassName('issue__files__bibtex')[0].textContent));
+		scrape(bibtex, url);
 	}
 }
 
-function scrape(doc, url) {
+function scrape(bibtex, url) {
 	item = new Zotero.Item("journalArticle");
-	const bibtex = (ZU.trimInternal(doc.getElementsByClassName('issue__files__bibtex')[0].value));
-	var author = bibtex.match(/author = \{(.+?)\}/)[1];
-	var title = bibtex.match(/title = \{(.+?)\}/)[1];
-	var subtitle = bibtex.match(/subtitle = \{(.+?)\}/)[1];
-	var journal = bibtex.match(/journal = \{(.+?)\}/)[1];
-	var volume = bibtex.match(/volume = \{(.+?)\}/)[1];
-	var year = bibtex.match(/year = \{(.+?)\}/)[1];
-	var pages = bibtex.match(/pages = \{(.+?)\}/)[1];
-	item.creators.push(ZU.cleanAuthor(author, 'Author', false));
-	item.title = subtitle;
-	item.shortTitle = title;
+	let author = bibtex.match(/author = \{(.+?)\}/);
+	let title = bibtex.match(/title = \{(.+?)\}/)[1];
+	let subtitle = bibtex.match(/subtitle = \{(.+?)\}/);
+	let journal = bibtex.match(/journal = \{(.+?)\}/)[1];
+	let volume = bibtex.match(/volume = \{(.+?)\}/)[1];
+	let year = bibtex.match(/year = \{(.+?)\}/)[1];
+	let pages = bibtex.match(/pages = \{(.+?)\}/)[1].replace('--', '-');
+	if (author) item.creators.push(ZU.cleanAuthor(author[1], 'author', false));
+	if (journal === 'iX') {
+		item.title = title;
+		if (subtitle) item.shortTitle = subtitle[1];
+	}
+	else {
+		item.title = subtitle !== null ? subtitle[1] : title;
+		if (subtitle) item.shortTitle = title;
+	}
 	item.publicationTitle = journal
 	item.volume = year;
 	item.issue = volume;
 	item.pages = pages;
 	item.url = url;
 
-	var pdfurl = url + "/pdf";
+	let pdfurl = url + "/pdf";
+	Zotero.debug(pdfurl);
 	item.attachments.push({
-	title:title,
-	filename:title + " - " + subtitle + " - " + author,
-	mimeType:"application/pdf",
-	url:pdfurl
+		title: title,
+		mimeType: "application/pdf",
+		url: pdfurl
 	});
 	item.complete();
 }
